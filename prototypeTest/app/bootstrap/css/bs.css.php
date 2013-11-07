2c837f2d46d07322443a7fab8a6c7b27e4279d93
Andy Southwick <andysouthwick@dgardner-r.local>
2013-11-07 16:47:04 -0700

    Added bootstrap styling

---
+++ prototypeTest/app/bootstrap/css/bs.css.php
@@ -0,0 +1,25 @@
+<!--bootstrap css-->
+<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.css">
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css" href="css/sticky-footer-navbar.css">
+<link rel="stylesheet" type="text/css" href="css/carousel.css">
+<? if ($page == NULL){echo'
+<link rel="stylesheet" type="text/css" href="css/navbar-fixed-top.css">';} ?>
+
+<!--close bootstrap css-->
+
+<!--vemma css-->
+
+
+<!--my css -->
+<link rel="stylesheet" type="text/css" href="css/mystyle.css">
+<style>
+body {
+        height: 100%;
+		background-color:<? 
+		if ($layout == 1){echo"#FFFFFF";}
+		if ($layout == 2){echo"#000000";}
+		 ?>;
+		background-repeat: repeat-x;
+}
+</style>
