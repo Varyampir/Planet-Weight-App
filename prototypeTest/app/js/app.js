4c80c4ca22b1d99735317bdaf9da9e4ac76223ed
joehafen <joehafen@gmail.com>
2013-11-19 21:51:17 -0700

    Redesigned navigation

--- prototypeTest/app/js/app.js
+++ prototypeTest/app/js/app.js
@@ -5,7 +5,9 @@
 angular.module('planetApp', ['ngRoute']).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.
-		when('/', {templateUrl: 'partials/enter-info.html'}).
+		when('/welcome', {templateUrl: 'partials/welcome.html'}).
+		when('/enter-info', {templateUrl: 'partials/enter-info.html', controller: 'planetData'}).
+		when('/show-planets', {templateUrl: 'partials/show-planets.html', controller: 'planetData'}).
 		when('/planets/:planetId', {templateUrl: 'partials/planet-detail.html', controller: 'planetData'}).
-		otherwise({redirectTo: '/'});
+		otherwise({redirectTo: '/welcome'});
 }]);
